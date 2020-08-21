1c8d7fda4b001c13d6e6f33a62ab5beac33d3123
Oscar Sanchez <osanchez@distritomoda.com.ar>
2020-08-21 11:53:45 -0300

    Update description

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,10 +1,5 @@
 import React from 'react'
-
-
-const Title = () => {
-  return (
-    <header className="my-5 w-100">
-      <h1 className="mb-5 text-white">"Generador aleatorio de tickets para DM-Drupal"</h1>
+ q qite">"Generador aleatorio de tickets para DM-Drupal"</h1>
     </header>
   )
 }
